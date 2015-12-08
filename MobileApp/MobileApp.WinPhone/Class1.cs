fbfabe8b0602e0d1170d9f2a4976b811fbc0e36c
Dmitry Bondarenko <bondarenkod@windowslive.com>
2015-12-08 23:59:18 +0200

    some code

---
+++ MobileApp/MobileApp.WinPhone/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace MobileApp.WinPhone
+{
+    public class Bootstrapper : PhoneBootstrapperBase
+    {
+        private PhoneContainer container;
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            container = new PhoneContainer();
+            container.RegisterPhoneServices(RootFrame);
+        }
+
+        protected override IEnumerable<Assembly> SelectAssemblies()
+        {
+            return new[]
+            {
+                GetType().Assembly,
+                typeof (LoginViewModel).Assembly
+            };
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            container.BuildUp(instance);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return container.GetAllInstances(service);
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return container.GetInstance(service, key);
+        }
+    }
+}
